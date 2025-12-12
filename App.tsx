5f1774376b25e44e81738b30b9eeb57965dafca2
secretsantamatch <santa@secretsantamatch.com>
2025-12-11 19:17:03 -0500

    sdfd

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import CreatePoll from './components/CreatePoll';
+import PollCreator from './components/PollCreator';
 import PollView from './components/PollView';
 
 const App: React.FC = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<CreatePoll />} />
+        <Route path="/" element={<PollCreator />} />
         <Route path="/poll/:id" element={<PollView />} />
       </Routes>
     </HashRouter>

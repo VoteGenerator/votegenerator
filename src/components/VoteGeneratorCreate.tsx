52f3d595b93e79fce637355821742b2ed6ec84b8
secretsantamatch <santa@secretsantamatch.com>
2025-12-13 08:23:41 -0500

    Update VoteGeneratorCreate.tsx

--- src/components/VoteGeneratorCreate.tsx
+++ src/components/VoteGeneratorCreate.tsx
@@ -255,7 +255,6 @@ const VoteGeneratorCreate: React.FC = () => {
             formData.append('file', compressedFile);
             formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET || 'votegenerator');
 
-        try {
             const res = await fetch(`https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`, {
                 method: 'POST',
                 body: formData
